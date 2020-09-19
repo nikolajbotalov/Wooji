a511ef53495082ddc175ca25396c382c0870783b
nikolajbotalov <nikolaj.botalov@gmail.com>
2020-09-19 19:46:17 +0500

    add filters

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import Products from './modules/Products.js';
 
+/**
+ * @author Nikolay Botalov <nikolaj.botalov@gmail.com> telegram <@nikolay696>
+ */
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
--- js/modules/Products.js
+++ js/modules/Products.js
@@ -1,9 +1,24 @@
+/**
+ * The class displays the company's products
+ * 
+ * @author Nikolay Botalov <nikolaj.botalov@gmail.com> telegram <@nikolay696>
+ */
 export default class Products {
+  /**
+   * Class constructor
+   * 
+   * @param {string} companySelector - block company
+   * @param {string} productsSelector - block's products
+   */
   constructor(companySelector, productsSelector) {
-    this.products = document.querySelectorAll(productsSelector);
     this.companyBtns = document.querySelector(companySelector);
+    this.products = document.querySelectorAll(productsSelector);
   } 
-
+  
+  /**
+   * 
+   * @param {string} product - class name of company's product;
+   */
   showProducts(product) {
     this.hideCompanies();
     let selector = '.' + product.slice(10); 
@@ -11,15 +26,28 @@ export default class Products {
     company.style.display = 'flex';
   }
 
+  /**
+   * hide all company's products
+   */
   hideCompanies() {
     this.products.forEach(product => {
       product.style.display = 'none';
     });
   }
 
+  /**
+   *  main method for class
+   */
   init() {
-    this.hideCompanies();
     
+    // hide all company's products
+    this.hideCompanies();
+
+    // show first company products
+    const company = document.querySelector('.diaso');
+    company.style.display = 'flex';
+    
+    // Сlick on the button, the company's products are displayed
     this.companyBtns.addEventListener('click', (e) => {
       let target = e.target; 
 
@@ -58,7 +86,6 @@ export default class Products {
           this.showProducts(target.className);
           break;           
         default:
-          this.showProducts('.companies-diaso');
           break;
       }
     });

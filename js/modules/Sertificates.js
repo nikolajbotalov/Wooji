fbae04ad50af2adfd5bf62bce4f41a7538d3d20d
nikolajbotalov <nikolaj.botalov@gmail.com>
2020-09-30 21:22:18 +0500

    block scroll when open modal

--- js/modules/Sertificates.js
+++ js/modules/Sertificates.js
@@ -1,31 +1,33 @@
 export default class Sertificates {
   constructor() {
-    this.certSection = document.querySelector(".certificates-slider");
-    this.bigImg = document.createElement("img");
-    this.modal = document.querySelector(".modal");
-    this.modalDialog = document.querySelector(".modal-dialog");
+    this.certSection = document.querySelector('.certificates-slider');
+    this.bigImg = document.createElement('img');
+    this.modal = document.querySelector('.modal');
+    this.modalDialog = document.querySelector('.modal-dialog');
   }
 
   openBigImage() {
     if (this.certSection === null) {
       return false;
     } else {
-      this.certSection.addEventListener("click", (e) => {
+      this.certSection.addEventListener('click', (e) => {
         let target = e.target;
 
-        if (target.className === "awards-slides") {
-          let imgSrc = target.getAttribute("src");
-          this.bigImg.classList.add("bigImage");
-          this.bigImg.setAttribute("src", imgSrc.replace("mini", "Big"));
-          this.modal.style.display = "block";
+        if (target.className === 'awards-slides') {
+          let imgSrc = target.getAttribute('src');
+          this.bigImg.classList.add('bigImage');
+          this.bigImg.setAttribute('src', imgSrc.replace('mini', 'Big'));
+          this.modal.style.display = 'block';
+          document.body.style.overflow = 'hidden';
           this.modalDialog.appendChild(this.bigImg);
         }
       });
     }
 
-    this.modal.addEventListener("click", () => {
-      this.bigImg.setAttribute("src", "");
-      this.modal.style.display = "none";
+    this.modal.addEventListener('click', () => {
+      this.bigImg.setAttribute('src', '');
+      this.modal.style.display = 'none';
+      document.body.style.overflow = '';
     });
   }
 }

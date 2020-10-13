a043ffcc1c3ec990ba1458284497118f52f8b9a8
nikolajbotalov <nikolaj.botalov@gmail.com>
2020-10-14 00:26:56 +0500

    linked filters from two pages

--- js/modules/Products.js
+++ js/modules/Products.js
@@ -11,19 +11,19 @@ export default class Products {
     this.filterBlocks = document.querySelectorAll(".filter");
     this.largeDesiccant = document.querySelector("div.large-desiccant");
     this.desiccant = document.querySelector("div.desiccant");
-    this.deodorant = document.querySelector('div.deodorant');
-    this.flavor = document.querySelector('div.flavor');
-    this.compound = document.querySelector('div.compound');
-    this.fruit_fly_trap = document.querySelector('div.fruit_fly_trap');
-    this.kitchen = document.querySelector('div.kitchen');
-    this.wardrobe = document.querySelector('div.wardrobe');
-    this.bathroom = document.querySelector('div.bathroom');
-    this.car = document.querySelector('div.car');
-    this.livingRoom = document.querySelector('div.living-room');
-    this.local = document.createElement('a');
-    this.git = document.createElement('a'); 
-    this.filterSection = document.querySelector('.filtres');
-    this.filterTabs = document.querySelector('.filter-tabs');
+    this.deodorant = document.querySelector("div.deodorant");
+    this.flavor = document.querySelector("div.flavor");
+    this.compound = document.querySelector("div.compound");
+    this.fruit_fly_trap = document.querySelector("div.fruit_fly_trap");
+    this.kitchen = document.querySelector("div.kitchen");
+    this.wardrobe = document.querySelector("div.wardrobe");
+    this.bathroom = document.querySelector("div.bathroom");
+    this.car = document.querySelector("div.car");
+    this.livingRoom = document.querySelector("div.living-room");
+    this.local = document.createElement("a");
+    this.git = document.createElement("a");
+    this.filterSection = document.querySelector(".filtres");
+    this.filterTabs = document.querySelector(".filter-tabs");
   }
 
   /**
@@ -42,17 +42,18 @@ export default class Products {
 
   /**
    * @param {string} product - class name sorted products
-   * @param {checkbox} checkboxSelector - current checkbox 
+   * @param {checkbox} checkboxSelector - current checkbox
    */
   showSortProduct(product, checkboxSelector) {
+    const sortedProducts = document.querySelector(`div.${product}`);
+
     this.hideCompanies();
     this.cancelChecked();
-
-    const sortedProducts = document.querySelector(`div.${product}`);
 
     if (sortedProducts !== null) {
       sortedProducts.style.display = "flex";
     } else {
+      console.log("false");
       return false;
     }
 
@@ -85,17 +86,36 @@ export default class Products {
     if (this.filterSection === null) {
       return false;
     } else {
-      this.filterTabs.children[1].style.display = 'none';
-      this.filterSection.addEventListener('click', (e) => {
-        let target = e.target; 
+      this.filterTabs.children[1].style.display = "none";
+      this.filterSection.addEventListener("click", (e) => {
+        let target = e.target;
 
-        if (target.className === 'filters-title') {
-          this.filterTabs.children[0].style.display = 'flex';
-          this.filterTabs.children[1].style.display = 'none';
-        } else if (target.className === 'room-filter') {
-          this.filterTabs.children[1].style.display = 'flex';
-          this.filterTabs.children[0].style.display = 'none';
-        } 
+        if (target.className === "filters-title") {
+          this.filterTabs.children[0].style.display = "flex";
+          this.filterTabs.children[1].style.display = "none";
+        } else if (target.className === "room-filter") {
+          this.filterTabs.children[1].style.display = "flex";
+          this.filterTabs.children[0].style.display = "none";
+        }
+      });
+    }
+  }
+
+  /**
+   * method check 
+   */
+  checkPage() {
+    if (sessionStorage.getItem("roomTitle") === null) {
+      this.hideCompanies();
+      this.cancelChecked();
+    } else {
+      this.checkboxes.forEach((checkbox) => {
+        let checkboxName = checkbox.nextElementSibling.textContent;
+
+        if (sessionStorage.getItem("roomTitle") === checkboxName) {
+          let checkboxClass = checkbox.nextElementSibling.className;
+          this.showSortProduct(checkboxClass, checkbox);
+        }
       });
     }
   }
@@ -106,108 +126,99 @@ export default class Products {
   init() {
     // if loc.href = index.html and click to room card,
     // room title save to localStorage
-    this.local = 'http://127.0.0.1:5500/index.html';
-    this.git = 'https://nikolajbotalov.github.io/Wooji/index.html'; 
+    this.local = "http://127.0.0.1:5500/index.html";
+    this.git = "https://nikolajbotalov.github.io/Wooji/index.html";
     if (location.href === this.local || location.href === this.git) {
-      const roomBtn = document.querySelector('.advantages-container');
-      roomBtn.addEventListener('click', (e) => {
+      const roomBtn = document.querySelector(".advantages-container");
+      roomBtn.addEventListener("click", (e) => {
         let target = e.target;
-        if (target.className == 'room-image') {
+        if (target.className == "room-image") {
           let roomTitle = target.nextElementSibling.children[0].textContent;
-          localStorage.setItem('roomTitle', roomTitle);
-        } else if (target.className === 'room-btn-title') {
-          let roomTitle = target.closest('.card-room').children[1].children[0].textContent;
-          localStorage.setItem('roomTitle', roomTitle);
+          sessionStorage.setItem("roomTitle", roomTitle);
+          location.href = "products.html";
+        } else if (target.className === "room-btn-title") {
+          let roomTitle = target.closest(".card-room").children[1].children[0].textContent;
+          sessionStorage.setItem("roomTitle", roomTitle);
+          location.href = "products.html";
         }
       });
     }
 
+    this.checkPage();
 
-    // hide all company's products & cancel checked checkboxes
-    this.hideCompanies();
-    this.cancelChecked();
     if (document.documentElement.clientWidth <= 415) {
       this.showCurrentFilterBlock();
     }
 
-    // show first company products
-    const company = document.querySelector(".diaso");
-    if (company === null) {
-      return false;
-    } else {
-      company.style.display = "flex";
-    }
-
-
     // Сlick on the button, the company's products are displayed
-    this.companyBtns.addEventListener("click", (e) => {
-      let target = e.target;
-      let selector = target.className.slice(10);
+    if (this.companyBtns !== null) {
+      this.companyBtns.addEventListener("click", (e) => {
+        let target = e.target;
+        let selector = target.className.slice(10);
 
-      this.showProducts(target.className);
-      localStorage.setItem('currentProducts', selector);
-    });
+        this.showProducts(target.className);
+        localStorage.setItem("currentProducts", selector);
+      });
+    }
 
     // Click on product card
     this.products.forEach((productCard) => {
-      productCard.addEventListener('click', (e) => {
+      productCard.addEventListener("click", (e) => {
         let target = e.target;
 
-        // if click on card image, set product name and image 
-        if (target.className === 'card-image') {
+        // if click on card image, set product name and image
+        if (target.className === "card-image") {
           let cardTitle = target.nextElementSibling.children[0].children[0].textContent;
           let cardDesc = target.nextElementSibling.children[0].children[1].textContent;
           let cardImage = target.src;
 
-          localStorage.setItem('productTitle', cardTitle);
-          localStorage.setItem('productDesc', cardDesc);
-          localStorage.setItem('productImage', cardImage);
-          location.href = 'goodsDetails.html';
-        } else if (target.className === 'more-details-text') {
-          let cardTitle = target.closest('.card-text').children[0].children[0].textContent;
-          let cardDesc = target.closest('.card-text').children[0].children[1].textContent;
-          let cardImage = target.closest('.card').children[0].src;
+          localStorage.setItem("productTitle", cardTitle);
+          localStorage.setItem("productDesc", cardDesc);
+          localStorage.setItem("productImage", cardImage);
+          location.href = "goodsDetails.html";
+        } else if (target.className === "more-details-text") {
+          let cardTitle = target.closest(".card-text").children[0].children[0].textContent;
+          let cardDesc = target.closest(".card-text").children[0].children[1].textContent;
+          let cardImage = target.closest(".card").children[0].src;
 
-          localStorage.setItem('productTitle', cardTitle);
-          localStorage.setItem('productDesc', cardDesc);
-          localStorage.setItem('productImage', cardImage);
-          location.href = 'goodsDetails.html';
+          localStorage.setItem("productTitle", cardTitle);
+          localStorage.setItem("productDesc", cardDesc);
+          localStorage.setItem("productImage", cardImage);
+          location.href = "goodsDetails.html";
         }
       });
     });
 
     //
     this.checkboxes.forEach((checkbox) => {
-      if (checkbox.nextElementSibling.className === 'large-desiccant') {
+      if (checkbox.nextElementSibling.className === "large-desiccant") {
         checkbox.nextElementSibling.textContent += ` (${this.largeDesiccant.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'desiccant') {
+      } else if (checkbox.nextElementSibling.className === "desiccant") {
         checkbox.nextElementSibling.textContent += ` (${this.desiccant.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'deodorant') {
+      } else if (checkbox.nextElementSibling.className === "deodorant") {
         checkbox.nextElementSibling.textContent += ` (${this.deodorant.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'flavor') {
+      } else if (checkbox.nextElementSibling.className === "flavor") {
         checkbox.nextElementSibling.textContent += ` (${this.flavor.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'compound') {
+      } else if (checkbox.nextElementSibling.className === "compound") {
         checkbox.nextElementSibling.textContent += ` (${this.compound.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'fruit_fly_trap') {
+      } else if (checkbox.nextElementSibling.className === "fruit_fly_trap") {
         checkbox.nextElementSibling.textContent += ` (${this.fruit_fly_trap.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'kitchen') {
+      } else if (checkbox.nextElementSibling.className === "kitchen") {
         checkbox.nextElementSibling.textContent += ` (${this.kitchen.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'wardrobe') {
+      } else if (checkbox.nextElementSibling.className === "wardrobe") {
         checkbox.nextElementSibling.textContent += ` (${this.wardrobe.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'bathroom') {
+      } else if (checkbox.nextElementSibling.className === "bathroom") {
         checkbox.nextElementSibling.textContent += ` (${this.bathroom.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'car') {
+      } else if (checkbox.nextElementSibling.className === "car") {
         checkbox.nextElementSibling.textContent += ` (${this.car.childElementCount})`;
-      } else if (checkbox.nextElementSibling.className === 'living-room') {
+      } else if (checkbox.nextElementSibling.className === "living-room") {
         checkbox.nextElementSibling.textContent += ` (${this.livingRoom.childElementCount})`;
       }
-
-    
 
       checkbox.addEventListener("click", (e) => {
         let target = e.target;
         this.showSortProduct(target.nextElementSibling.className, checkbox);
-        localStorage.setItem('currentProducts', target.nextElementSibling.className);
+        localStorage.setItem("currentProducts", target.nextElementSibling.className);
       });
     });
   }
